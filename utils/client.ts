3f4c0f946638757640fe3d77fc7305dbf2257fcc
Tan Tai <taib1910442@student.ctu.edu.vn>
2023-02-27 11:29:25 +0700

    test

--- utils/client.ts
+++ utils/client.ts
@@ -1,9 +1,9 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+  projectId: 'nmour9po',
   dataset: 'production',
-  apiVersion: '2022-03-10',
+  apiVersion: '2023-02-27',
   useCdn: false,
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  token: 'skA1l9WBcWmdglRg7a2axfI4O9vYL8rHftjz0VRRQVbC0VN4YBADXXAHEM8OS9KS2Xi9Mh6hLnri8VHdtNnSYCOvROuaD7cI3VKst5Qbv2cnoYVmdsDQ8uwwXduNAOegyagqKaF8gF9FlRTiblOq9QRKCJFwTYACdT6N0zLQCj6CoTC1sgx2',
 });
