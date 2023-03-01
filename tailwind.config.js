3e5388b908f4634e75fed992fb5e08cc4ff3b904
Tan Tai <taib1910442@student.ctu.edu.vn>
2023-03-01 20:35:02 +0700

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,7 @@ module.exports = {
       },
       backgroundImage: {
         'blurred-img':
-          "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsaaJ7s4lqcBF4IDROVPzrlL5fexcwRmDlnuEYQenWTt1DejFY5kmYDref2a0Hp2eE4aw&usqp=CAU')",
+          "url('https://live.staticflickr.com/7592/16888464777_0073470e81_b.jpg')",
       },
     },
   },
